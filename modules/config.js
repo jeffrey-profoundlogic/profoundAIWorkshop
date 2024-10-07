833f84e356ca4a1109e64247538669e07c3383c7
Jeffrey Williams <jwilliams@profoundlogic.com>
2024-10-07 15:37:14 -0500

    added models to config.js

--- modules/config.js
+++ modules/config.js
@@ -56,5 +56,25 @@ module.exports = {
   "connectorIPFilter": (ip) => true,
   "showIBMiParmDefn": true,
   "userdataDirectory": path.resolve(currentDir, "../userdata"),
-  "modulesDirectory": path.resolve(currentDir, "../modules")
+  "modulesDirectory": path.resolve(currentDir, "../modules"),
+  models: {
+    "GPT-4o": {
+      provider: "openai",
+      model: "gpt-4o",
+      apiKey: process.env.OPENAI_API_KEY,
+      defaultAssistantId: "asst_msrGJAgAdnR8qZYCQ2SoqEBO",
+      stream: true
+    },
+    "GPT-4o mini": {
+      provider: "openai",
+      model: "gpt-4o-mini",
+      apiKey: process.env.OPENAI_API_KEY,
+      stream: true
+    },
+    "GPT-3.5 Turbo": {
+      provider: "openai",
+      model: "gpt-3.5-turbo-0125",
+      apiKey: process.env.OPENAI_API_KEY,
+    },
+  }
 }
